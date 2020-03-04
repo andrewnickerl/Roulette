04482f1159fc7346d39aac40069637c060e8c86f
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-03-04 07:24:46 -0500

    Completed switch statement for WinningBets, added comments to Main()

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 
+/*This program simulates a roulette wheel and returns all the winning
+ * bets for each spin of the wheel by using an RNG to generate a Bin
+ * object using the Spin method of the Wheel class, and then passing that
+ * Bin object to the Wheel class method WinningBets which returns a string
+ * of all the winning bets that correspond to that Bin.
+ */
+
 namespace Roulette
 {
     class Program
@@ -8,8 +15,9 @@ namespace Roulette
         {
             Wheel wheel = new Wheel();
             Bin bin = wheel.Spin(wheel.Numbers, wheel.Colors);
+
             Console.WriteLine($"The winning bin is {bin.Number}!");
-            Console.WriteLine("The winning bets are:");
+            Console.WriteLine("\nThe winning bets are:\n");
             Console.Write(wheel.WinningBets(bin));
         }
     }
