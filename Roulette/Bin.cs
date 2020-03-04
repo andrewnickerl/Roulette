56e2dcf0a60f98c399ccabf7f1d530a3e5786d10
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-03-03 20:50:56 -0500

    Create Bin.cs and add Number and Color properties to be set through the added constructor

---
+++ Roulette/Bin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roulette
+{
+    class Bin
+    {
+        public Bin(int number, string color)
+        {
+            this.Number = number;
+            this.Color = color;
+        }
+
+        public int Number { get; set; }
+        public string Color { get; set; }
+    }
+}
