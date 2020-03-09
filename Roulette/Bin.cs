b1cfc1f2f04bb83206cf7e64e608cc78d0dc5cb4
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-03-09 10:48:49 -0400

    Added WinningBets method to Bin class utilizing the newly created methods from the Bets class in order to remove the large amount of string hard coding written into the WinningBets method

--- Roulette/Bin.cs
+++ Roulette/Bin.cs
@@ -14,5 +14,23 @@ namespace Roulette
 
         public string Number { get; set; }
         public string Color { get; set; }
+
+        public string WinningBets(Bin bin)
+        {
+            Bets bet = new Bets();
+            string winningBets = "";
+
+            winningBets += bet.NumBet(bin);
+            winningBets += "\n" + bet.EvenOddBet(bin);
+            winningBets += "\n" + bet.RedBlackBet(bin);
+            winningBets += "\n" + bet.LowHighBet(bin);
+            winningBets += "\n" + bet.DozensBet(bin);
+            winningBets += "\n" + bet.ColumnsBet(bin);
+            winningBets += "\n" + bet.StreetBet(bin);
+            winningBets += "\n" + bet.SixNumberBet(bin);
+            winningBets += "\n" + bet.SplitBet(bin);
+            winningBets += "\n" + bet.CornerBets(bin);
+            return winningBets;
+        }
     }
 }
